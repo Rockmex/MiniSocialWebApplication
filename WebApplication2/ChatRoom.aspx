b7ec8b25ccedaa175ac20f07b32b2c482f191f60
Rockmex <romerchia@hotmail.com>
2020-06-30 17:29:46 -0400

    Added ChatRoom.aspx

---
+++ WebApplication2/ChatRoom.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="ChatRoom.aspx.cs" Inherits="WebApplication2.ChatRoom" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="rg_layout">
+		    <div class="rg_center">
+                <div class="rg_form">
+                    <div class="word">
+                    <table>
+                        <tr>
+                            <td colspan="2" class="td_center"><asp:Label ID="Label_display" Text="Chat Room" runat="server" ></asp:Label></td>
+                        </tr>                           
+                    </table>
+                        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                            <Columns>
+                                <asp:BoundField DataField="MessageId" HeaderText="MessageId" visible="False"/>
+                                <asp:BoundField DataField="SenderId" HeaderText="SenderId" />
+                                <asp:BoundField DataField="Message" HeaderText="Message" />
+                                <asp:BoundField DataField="Time" HeaderText="Time" />
+                                <asp:TemplateField>
+                                        <ItemTemplate>
+                                            <asp:Button ID="Button2" runat="server" Text="Delete" CommandArgument='<%# Eval("MessageId") %>' OnCommand="Button_Click_Delete"/>
+                                        </ItemTemplate>
+                                </asp:TemplateField>
+
+                            </Columns>
+                        </asp:GridView>
+                        <table>
+                            <tr>
+                                <td><asp:TextBox ID="MessageBox" runat="server"></asp:TextBox></td>
+                                <td><asp:Button ID="Button1" runat="server" Text="Send" onclick="Button_Click_Send"/></td>
+                            </tr>                         
+                        </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</asp:Content>
---
+++ WebApplication2/ChatRoom.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class ChatRoom : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (CheckRoom() == 0)
+                {
+                    Response.Write("You are not in this Chat Room");
+                    Response.Redirect("Home.aspx");
+                }
+                else
+                {
+                    if (Count() == 0)
+                    {
+                        GridView1.Visible = false;
+                        Label_display.Text = "This is your first time chatting. Please write something.";
+                    }
+                    else
+                    {
+                        ShowResult();
+                        Label_display.Text = getRoomName();
+                    }
+                }
+            }
+        }
+        protected void Button_Click_Send(object sender, EventArgs e)
+        {
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+            {
+                conn.Open();
+
+                string insertQuery = "insert into ChatLog (SenderId, Message, ReceiverId,Time) values ('" + Session["UID"] + "', @Msg,'" + Session["RoomId"] + "', getdate())";
+
+                SqlCommand cmdInsert = new SqlCommand(insertQuery, conn);
+
+                cmdInsert.Parameters.AddWithValue("@Msg", MessageBox.Text);
+
+                cmdInsert.ExecuteNonQuery();
+
+                conn.Close();
+            }
+
+            Response.Redirect("ChatRoom.aspx");
+        }
+
+        protected void Button_Click_Delete(object sender, CommandEventArgs e)
+        {
+            Session["MID"] = e.CommandArgument;
+
+            if (e.CommandArgument != null)
+            {
+                using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+                {
+                    conn.Open();
+
+                    string deleteQuery = "DELETE FROM ChatLog WHERE MessageId = '" + Session["MID"] + "'";
+
+                    SqlCommand cmdDelete = new SqlCommand(deleteQuery, conn);
+
+                    cmdDelete.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+
+                Response.Redirect("ChatRoom.aspx");
+            }
+            else
+            {
+                Response.Redirect("Home.aspx");
+            }
+        }
+        private void ShowResult()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string searchCmd = "SELECT MessageId, SenderId, Message, Time FROM ChatLog WHERE ReceiverId = '" + Session["RoomId"] + "' ORDER BY TIME ASC";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(searchCmd, conn);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+        private int Count()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string searchCmd = "SELECT Count(*) FROM ChatLog WHERE ReceiverId = '" + Session["RoomId"] + "'";
+            SqlCommand cmdCheck = new SqlCommand(searchCmd, conn);
+            return Convert.ToInt32(cmdCheck.ExecuteScalar().ToString());
+
+        }
+
+        private int CheckRoom()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string searchCmd = "SELECT Count(*) FROM ChatRoom WHERE IDwithChar = '" + Session["RoomId"] + "' AND MemberId = " + Session["UID"];
+            SqlCommand cmdCheck = new SqlCommand(searchCmd, conn);
+            return Convert.ToInt32(cmdCheck.ExecuteScalar().ToString());
+
+        }
+
+        private String getRoomName()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string searchCmd = "SELECT RoomName FROM ChatRoom WHERE IDwithChar = '" + Session["RoomId"] + "'";
+            SqlCommand cmdCheck = new SqlCommand(searchCmd, conn);
+            return cmdCheck.ExecuteScalar().ToString();
+        }
+    }
+}
